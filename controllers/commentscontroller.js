3e52f8e35e4557b9952d662760d7a9bdb36fbdc9
Patrick Cook <masonobledev@gmail.com>
2022-01-27 13:50:39 -0500

    first push

--- controllers/commentscontroller.js
+++ controllers/commentscontroller.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 const { models } = require('../models');
 
-//
+router.post('/comment', async (req, res) => {
+
+    const {content, postID} = req.body.comment;
+
+    try {
+        await models.CommentsModel.create({
+            content: content,
+            postID: postID,
+            userID: req.user.id
+        })
+        .then(
+            comment => {
+                res.status(201).json({
+                    comment: comment,
+                    message: 'comment created'
+                });
+            }
+        )
+    } catch (err) {
+        res.status(500).json({
+            error: 'Failed to create comment: ${err}'
+        });
+    };
+});
 
 module.exports = router;
--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -4,6 +4,54 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UniqueConstraintError } = require('sequelize/lib/errors');
 
-//
+router.post('/signup' , async (req, res) =>{
+    const {username, password} = req.body.user;
+    try {
+        await models.UsersModel.create({
+            username: username,
+            password: bcrypt.hashSync(password, 10)
+        })
+        .then(
+            user => {
+                let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+                res.status(201).json({
+                    user: user,
+                    message: 'user created',
+                    sessionToken: 'Bearer ${token}'
+                });
+            }
+        )
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({ 
+                message: 'Username already in use'
+            });
+        } else {
+            res.status(500).json({
+                error: `Failed to register user: ${err}`
+            });
+        };
+    };
+});
+
+// We're going to make a get request in userscontroller.js that will show us not only all of the Users stored in the database, but any associated Posts and Comments with those Users, as well. 
+
+
+router.get('/userinfo', async (req, res) => {
+    try {
+        await models.UsersModel.findAll()
+        .then(
+            users => {
+                res.status(200).json({
+                    users: users
+                });
+            }
+        )
+    } catch (err) {
+        res.status(500).json({
+            error: `Failed to retrieve users: ${err}`
+        });
+    };
+});
 
 module.exports = router;
